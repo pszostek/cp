821c90d488b055c2c86aff30ea9efb71bf7a0e2b
xs-exp <none@none.org>
2013-07-18 18:38:21 +0200

    add demangling C binding

---
+++ cp/demangle/CMakeFiles/demangle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demangle.dir/demangle.cxx.o"
+  "libdemangle.pdb"
+  "libdemangle.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/demangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
