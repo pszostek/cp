f18eb6d634a7c7ba83e392c8d22203d2c62c7e55
Pawel Szostek <pawel.szostek@cern.ch>
2013-09-18 14:18:25 +0200

    cosmetic changes

--- cp/xed-ex1.cpp
+++ cp/xed-ex1.cpp
@@ -11,30 +11,30 @@ extern "C" {
 #include <cstdlib>
 using namespace std;
 
-int main(int argc, char** argv);
+//
+// usage: ./xed-ex1 [32,64] /path/to/dumped/.text/section
+//
 
-int main(int argc, char** argv) {
+int disassemble(int mode, char* data, unsigned int length) {
     xed_bool_t long_mode;
     xed_state_t dstate;
     int first_argv;
-    int bytes = 0;
     int i;
     unsigned int u;
     xed_decoded_inst_t xedd;
-#define BUFLEN  1000
+    const unsigned int BUFLEN = 1000;
     char buffer[BUFLEN];
 
     xed_tables_init();
     xed_state_zero(&dstate);
     xed_set_verbosity( 99 );
-
-    if (argc > 2 && strcmp(argv[1], "64") == 0) {
+    if (mode == 64) {
         long_mode = true;
-    } else if(strcmp(argv[1], "32") == 0) {
+    } else if(mode = 32) {
         long_mode = false;
     } else {
         cerr << "Arg #1 must be equal to 32 or 64." << endl;
-        exit(1);
+        return 1;
     }
 
     if (long_mode)  {
@@ -48,41 +48,29 @@ int main(int argc, char** argv) {
     }
 
     // read .text section from a file
-    ifstream input_file;
+    cout << length << " bytes" << endl;
 
-    input_file.open(argv[2], ifstream::in | ifstream::binary);
-    input_file.seekg (0, input_file.end);
-    bytes = input_file.tellg();
-    input_file.seekg (0, input_file.beg);
-
-    cout << bytes << " bytes" << endl;
-
-    if (bytes == 0) {
-        cout << "Must supply non-empty input file" << endl;
-        exit(1);
+    if (length == 0) {
+        cout << "Must supply non-empty input string" << endl;
+        return 2;
     }
-    xed_uint8_t *itext = (xed_uint8_t*) malloc(bytes);
-    if(itext == NULL) {
-        exit(2);
-    }
-    input_file.read((char*)(itext), bytes);
 
     uint32_t start = 0, stop = 1;
     uint32_t inst_count = 0;
 
     double start_time = omp_get_wtime();
-    while(start < bytes && stop <= bytes) {
+    while(start < length && stop <= length) {
         xed_decoded_inst_zero_set_mode(&xedd, &dstate);
         // cout << "start: " << start << " stop: " << stop << endl;
         //     cout << hex << setw(2) << setfill('0')
         //          << XED_STATIC_CAST(unsigned int,itext[start]) << " ";
         // cout << endl << setfill(' ');
-         // for(int i=start; i<stop; ++i) {
-         //     cout << hex << setw(2) << XED_STATIC_CAST(unsigned int, itext[i]) << " ";
-         // } cout << endl;
+         for(int i=start; i<stop; ++i) {
+             cout << hex << setw(2) << XED_STATIC_CAST(unsigned int, data[i]) << " ";
+         } cout << endl;
 
         xed_error_enum_t xed_error = xed_decode(&xedd, 
-                                                XED_REINTERPRET_CAST(xed_uint8_t*,itext+start),
+                                                XED_REINTERPRET_CAST(xed_uint8_t*,data+start),
                                                 stop-start);
         switch(xed_error)
         {
@@ -98,7 +86,7 @@ int main(int argc, char** argv) {
             stop += 1;
             break;
           case XED_ERROR_GENERAL_ERROR:
-            cerr << "Could not decode given input: XED general error" << endl;
+            // cerr << "Could not decode given input: XED general error" << endl;
             stop += 1;
             break;
           default:
@@ -112,14 +100,15 @@ int main(int argc, char** argv) {
     double end_time = omp_get_wtime();
     cout << inst_count << " instructions" << endl;  
     cout << end_time - start_time << " seconds" << endl;
-    // xed_bool_t ok;
-    // for(u=  XED_SYNTAX_XED; u < XED_SYNTAX_LAST; u++) {
-    //     xed_syntax_enum_t syntax = static_cast<xed_syntax_enum_t>(u);
-    //     ok = xed_format(syntax, &xedd, buffer, BUFLEN, 0);
-    //     if (ok)
-    //         cout << xed_syntax_enum_t2str(syntax) << " syntax: "  << buffer << endl;
-    //     else
-    //         cout << "Error disassembling " << xed_syntax_enum_t2str(syntax) << " syntax" << endl;
-    // }
     return 0;
+}
+
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv) {
+
+
+    int retcode = disassemble(atoi(argv[1]), argv[3], atoi(argv[2]));
+    return retcode;
 }
