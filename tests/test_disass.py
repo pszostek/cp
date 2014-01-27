5c2afea8c60a459bc5335b3a59f1b6c0c4320aa5
Pawel Szostek <pawel.szostek@cern.ch>
2014-01-27 17:47:32 +0100

    add a test to disass module

---
+++ tests/test_disass.py
@@ -0,0 +1,23 @@
+import xed
+import disass
+import elf as elf
+import unittest
+
+class TestDisass(unittest.TestCase):
+    def setUp(self):
+        self.elf = elf.ELFFile("files/test_elf")
+    
+    def test_get_basic_block(self):
+        bb = disass.get_basic_block(self.elf, 0x81c)
+        self.assertTrue(isinstance(bb, xed.inst_list_t))
+        self.assertTrue(len(bb) > 0 )
+        self.assertTrue(xed.terminates_bb(bb[-1]))
+
+    def test_get_basic_blocks(self):
+        bbs = disass.get_basic_blocks(self.elf, [0x964, 0x97a, 0x992, 0x7f4])
+        for bb in bbs:
+            self.assertTrue(xed.terminates_bb(bb[-1]))
+            self.assertTrue(len(bb) > 0)
+
+if __name__ == "__main__":
+    unittest.main()
