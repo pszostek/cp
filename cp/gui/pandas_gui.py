4f23de91d9b4c4c3ed5ca155555b48c1d6be111a
xs-exp <none@none.org>
2014-02-04 16:53:43 +0100

    move UIloader to a separate file

--- cp/gui/pandas_gui.py
+++ cp/gui/pandas_gui.py
@@ -9,33 +9,8 @@ from pandas import *
 import random
 import time
 randn = np.random.randn
+from uiloader import loadUi
 
-
-class UiLoader(QUiLoader):
-    def __init__(self, baseinstance):
-        QUiLoader.__init__(self, baseinstance)
-        self.baseinstance = baseinstance
-
-    def createWidget(self, class_name, parent=None, name=''):
-        if parent is None and self.baseinstance:
-            # supposed to create the top-level widget, return the base instance
-            # instead
-            return self.baseinstance
-        else:
-            # create a new widget for child widgets
-            widget = QUiLoader.createWidget(self, class_name, parent, name)
-            if self.baseinstance:
-                # set an attribute for the new child widget on the base
-                # instance, just like PyQt4.uic.loadUi does.
-                setattr(self.baseinstance, name, widget)
-            return widget
-
-
-def loadUi(uifile, baseinstance=None):
-    loader = UiLoader(baseinstance)
-    widget = loader.load(uifile)
-    QMetaObject.connectSlotsByName(widget)
-    return widget
 
 class MainWindow(QMainWindow):
 
