559054b0a74b0654f2cfc66a6f0b1c9261a3b509
xs-exp <none@none.org>
2014-05-27 17:11:29 +0200

    fix broken paths

--- cp/sourceme.sh
+++ cp/sourceme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export LD_LIBRARY_PATH=$PWD/gui/hierarchical_header/libhhv/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$PWD/../cp/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/../lib:$LD_LIBRARY_PATH
