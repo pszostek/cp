436bb28d9398cf8a0efb855ad584c469fa9ebbf3
Pawel Szostek <pawel.szostek@cern.ch>
2016-08-10 20:15:23 +0200

    Add compiler and virtualenv to the env setup script

--- cp/sourceme.sh
+++ cp/sourceme.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+source /afs/cern.ch/sw/lcg/external/gcc/5.2.0/x86_64-slc6/setup.sh
+source ~/cp/bin/activate
+
 export LD_LIBRARY_PATH=$PWD/gui/hierarchical_header/libhhv/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$PWD/../lib:$LD_LIBRARY_PATH
