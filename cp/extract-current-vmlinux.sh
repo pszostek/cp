0ccbe7b80b10ab7bb5e68ad40dd4062f53da02a0
Mysterious Tur <none@none.org>
2016-08-22 00:01:13 +0200

    improved current extraction script

--- cp/extract-current-vmlinux.sh
+++ cp/extract-current-vmlinux.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# this script extracts the current kernel version and system.map
+
+# https://en.wikipedia.org/wiki/System.map - a guide to symbol types.
+#	filtering out absolute and data symbols (these are prime suspects
+#	as they fall out of the 0xfff ... address range we will be looking at
+
 ME=$(readlink -f "$0")
 MYPATH=$(dirname "$ME")
 
 $MYPATH/extract-vmlinux.sh /boot/vmlinuz-`uname -r` > /tmp/vmlinux.elf
-cp -f /boot/System.map-`uname -r` /tmp/vmlinux.symbols
+cat /boot/System.map-`uname -r` | grep -v "A" | grep -v "D" | grep -v "d" > /tmp/vmlinux.symbols
